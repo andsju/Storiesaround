7aa4662e5758de5526c064db516c38aa2f16598b
Anders Sjunnesson <anders.sjunnesson@gmail.com>
2017-11-30 07:41:47 +0100

    Grid input validation

--- cms/libraries/js/functions.js
+++ cms/libraries/js/functions.js
@@ -661,3 +661,46 @@ function getSelectStrings(arrayOfStrings, current, name, id, classes) {
 	html += "</select>";
 	return html;
 }
+
+
+/**
+ * Function to validate data
+ *
+ * @param data
+ * @param type
+ * @returns {boolean}
+ */
+function validateThis(data, type) {
+
+	var pattern;
+	type.toLowerCase();
+
+	switch(type) {
+		case "username" : // 4-8 length
+			pattern = /^(?=.{4,8}$)[A-Za-z0-9]+$/;
+			break;
+		case "numbers" :
+			pattern = /^[0-9]+$/;
+			break;
+		case "anynumbers" :
+			pattern = /\d+/;
+			break;
+		case "email" :
+			// code from http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+			pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			break;
+		case "url" :
+			// code from https://stackoverflow.com/questions/161738/what-is-the-best-regular-expression-to-check-if-a-string-is-a-valid-url
+			pattern = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+			break;
+		case "heading" : // max 100 length
+			pattern = /^(?=.{2,100}$)[\s\wåäöÅÄÖ]+$/;
+			break;
+	}
+
+	if (pattern === undefined) {
+		return false;
+	}
+
+	return pattern.test(data);
+}
