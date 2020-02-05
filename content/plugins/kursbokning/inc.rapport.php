512c7e4330d7f7745378508f4b3225a64cce3904
Anders Sjunnesson <anders.sjunnesson@gmail.com>
2020-02-05 09:19:12 +0100

    Remove directory

--- content/plugins/kursbokning/inc.rapport.php
+++
@@ -1,6 +0,0 @@
-<option value=""></option>
-<option value="/rapport/word_lista_1.php">Word lista</option>
-<option value="/rapport/word_rapport_1.php">Word rapport</option>
-<option value="/rapport/word_rapport_2.php">Word rapport + frågor</option>
-<option value="/rapport/excel_rapport.php">Excel alla uppgifter</option>
-<option value="/rapport/excel_rapport_avanti.php">Excel export till Avanti</option>
