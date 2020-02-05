e3f41fe009c5b63ccfc12d05acc7a181cb3226b0
Anders Sjunnesson <anders.sjunnesson@gmail.com>
2020-02-05 16:23:54 +0100

    Install updates

--- cms/includes/inc.header.php
+++ cms/includes/inc.header.php
@@ -6,7 +6,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <?php echo $_SESSION['site_script']; ?>
+    <?php /* echo $_SESSION['site_script']; */ ?>
 
     <?php   
     if (isset($meta_keywords)) {
